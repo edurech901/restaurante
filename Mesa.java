c073b10cd024296cfcc321ffaa0a4ad7a1e83376
edurech901 <eduardo.rech901@gmail.com>
2024-09-26 11:03:16 -0300

    atributos-classes 2

--- Mesa.java
+++ Mesa.java
@@ -1,3 +1,12 @@
 public class Mesa {
+  int num_mesa;
+  int capacidade;
+  boolean status;
   
+  public Mesa(int num_mesa, int capacidade){
+    this.num_mesa = num_mesa;
+    this.capacidade = capacidade;
+    this.status = false;
+  }
+
 }
