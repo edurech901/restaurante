9180f2d1929f7c13be68afdd46b6005c4bfadf7e
edurech901 <eduardo.rech901@gmail.com>
2024-09-26 11:02:40 -0300

    atributos-classes 1

--- Funcionario.java
+++ Funcionario.java
@@ -1,3 +1,15 @@
 public class Funcionario {
-  
+  String nome;
+  int id;
+  String cargo;
+  int pedidos;
+  Double valor_vendido;
+
+  public Funcionario(String nome, int id, String cargo){
+    this.nome = nome;
+    this.id = id;
+    this.cargo = cargo;
+    this.pedidos = 0;
+    this.valor_vendido = 0.0;
+  }
 }
