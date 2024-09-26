51f42692a4ec84cb5b14c818e1237a98f4cad0ae
edurech901 <eduardo.rech901@gmail.com>
2024-09-26 20:18:56 -0300

    lista_mesa 1

--- Pedido.java
+++ Pedido.java
@@ -2,40 +2,59 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Pedido {
-    private Mesa mesa;
-    private Funcionario garcom;
-    private List<Cardapio> itens;
-    private double valorTotal;
+  private Mesa mesa;
+  private Funcionario garcom;
+  private List<Cardapio> itens;
+  private double valorTotal;
 
-    public Pedido(Mesa mesa, Funcionario garcom) {
-        this.mesa = mesa;
-        this.garcom = garcom;
-        this.itens = new ArrayList<>();
-        this.valorTotal = 0.0;
+  public Pedido(Mesa mesa, Funcionario garcom) {
+    this.mesa = mesa;
+    this.garcom = garcom;
+    this.itens = new ArrayList<>();
+    this.valorTotal = 0.0;
+  }
+  
+  public Funcionario getGarcom() {
+    return garcom;
+  }
+  public void setGarcom(Funcionario garcom) {
+    this.garcom = garcom;
+  }
+  public Mesa getMesa() {
+    return mesa;
+  }
+  public void setMesa(Mesa mesa) {
+    this.mesa = mesa;
+  }
+  public List<Cardapio> getItens() {
+    return itens;
+  }
+  public void setItens(List<Cardapio> itens) {
+    this.itens = itens;
+  }
+
+  public void adicionarItem(Cardapio item) {
+    if (item.estoque > 0) {
+      itens.add(item);
+      valorTotal += item.preco;
+      item.estoque--;
+      System.out.println("Item " + item.nome_prato_bebida + " adicionado ao pedido.");
+    } else {
+      System.out.println("O item " + item.nome_prato_bebida + " está esgotado.");
     }
+  }
 
-    public void adicionarItem(Cardapio item) {
-        if (item.estoque > 0) {
-            itens.add(item);
-            valorTotal += item.preco;
-            item.estoque--; 
-            System.out.println("Item " + item.nome_prato_bebida + " adicionado ao pedido.");
-        } else {
-            System.out.println("O item " + item.nome_prato_bebida + " está esgotado.");
-        }
+  public void mostrarValorTotal() {
+    System.out.println("Valor total do pedido: R$ " + valorTotal);
+  }
+
+  public void detalhesPedido() {
+    System.out.println("Mesa: " + mesa.num_mesa);
+    System.out.println("Garçom: " + garcom.nome);
+    System.out.println("Itens do pedido:");
+    for (Cardapio item : itens) {
+      System.out.println("- " + item.nome_prato_bebida + ": R$ " + item.preco);
     }
-
-    public void mostrarValorTotal() {
-        System.out.println("Valor total do pedido: R$ " + valorTotal);
-    }
-
-    public void detalhesPedido() {
-        System.out.println("Mesa: " + mesa.num_mesa);
-        System.out.println("Garçom: " + garcom.nome);
-        System.out.println("Itens do pedido:");
-        for (Cardapio item : itens) {
-            System.out.println("- " + item.nome_prato_bebida + ": R$ " + item.preco);
-        }
-        mostrarValorTotal();
+      mostrarValorTotal();
     }
 }
--- Restaurante.java
+++ Restaurante.java
@@ -3,30 +3,40 @@ import java.util.List;
 
 public class Restaurante {
 
+  public static List<Cardapio> cardapio(){
+    List<Cardapio> cardapio = new ArrayList<>();
+
+    cardapio.add(new Cardapio("Jen's Cheese", 1, 24.00));
+    cardapio.add(new Cardapio("Jen's Salada", 2, 26.00));
+    cardapio.add(new Cardapio("Jen's Cheddar", 3, 26.00));
+    cardapio.add(new Cardapio("Jen's Bacon", 4, 28.00));
+    cardapio.add(new Cardapio("Jen's Crispy", 5, 29.00));
+    cardapio.add(new Cardapio("Jen's Mexican", 6, 28.00));
+    cardapio.add(new Cardapio("Jen's Clássico", 7, 29.00));
+    cardapio.add(new Cardapio("Jen's Sweet Pepper", 8, 29.00));
+    cardapio.add(new Cardapio("Jen's Cheddar 2.0", 9, 31.00));
+    cardapio.add(new Cardapio("Jen's Tomato Dry", 10, 29.00));
+    cardapio.add(new Cardapio("Big Jen's", 11, 29.90));
+    cardapio.add(new Cardapio("Bacon Jelly", 12, 37.90));
+    cardapio.add(new Cardapio("Jen's Costela Alhoó", 13, 37.90));
+    cardapio.add(new Cardapio("Jen's Costela BBQ", 14, 38.80));
+    cardapio.add(new Cardapio("Duplo BCC", 15, 39.90));
+    return cardapio;
+  }
+
+  public static List<Mesa> mesas(){
+    List<Mesa> mesas = new ArrayList<>();
+    mesas.add(new Mesa(1, 4));
+    mesas.add(new Mesa(2, 4));
+    mesas.add(new Mesa(3, 4));
+    mesas.add(new Mesa(4, 6));
+    mesas.add(new Mesa(5, 10));
+    return mesas;
+  }
+
   public static void main(String[] args) {
     
-  List<Cardapio> cardapio = new ArrayList<>();
-
-        cardapio.add(new Cardapio("Jen's Cheese", 1, 24.00));
-        cardapio.add(new Cardapio("Jen's Salada", 2, 26.00));
-        cardapio.add(new Cardapio("Jen's Cheddar", 3, 26.00));
-        cardapio.add(new Cardapio("Jen's Bacon", 4, 28.00));
-        cardapio.add(new Cardapio("Jen's Crispy", 5, 29.00));
-        cardapio.add(new Cardapio("Jen's Mexican", 6, 28.00));
-        cardapio.add(new Cardapio("Jen's Clássico", 7, 29.00));
-        cardapio.add(new Cardapio("Jen's Sweet Pepper", 8, 29.00));
-        cardapio.add(new Cardapio("Jen's Cheddar 2.0", 9, 31.00));
-        cardapio.add(new Cardapio("Jen's Tomato Dry", 10, 29.00));
-        cardapio.add(new Cardapio("Big Jen's", 11, 29.90));
-        cardapio.add(new Cardapio("Bacon Jelly", 12, 37.90));
-        cardapio.add(new Cardapio("Jen's Costela Alhoó", 13, 37.90));
-        cardapio.add(new Cardapio("Jen's Costela BBQ", 14, 38.80));
-        cardapio.add(new Cardapio("Duplo BCC", 15, 39.90));
-        
-        for (Cardapio item : cardapio) {
-            System.out.println(item);
-        }
+    
+    
     }
-
-  
 }
