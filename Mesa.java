8b2ed94ad0820b5b0161850ce4fe6dedbbf6acd3
edurech901 <eduardo.rech901@gmail.com>
2024-09-26 11:23:14 -0300

    mesa-funções 1

--- Mesa.java
+++ Mesa.java
@@ -9,4 +9,20 @@ public class Mesa {
     this.status = false;
   }
 
+  public void consultar_status(){
+    if(status == true) System.out.println("A mesa " + this.num_mesa + " esta disponivel");
+    else System.out.println("O prato " + this.num_mesa + " nao esta disponivel");
+  }
+
+  public void mudar_status(){
+    if(status == false) {
+      status = true;
+      System.out.println("A mesa " + this.num_mesa + " ficou disponivel");
+    }
+    else if(status == true) {
+      status = false;
+      System.out.println("A mesa " + this.num_mesa + " nao esta mais disponivel");
+    }
+  }
+
 }
