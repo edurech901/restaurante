82bb05eb129290e1f3114aa2a7a808f0651695f9
edurech901 <eduardo.rech901@gmail.com>
2024-09-27 23:41:38 -0300

    correção main 1

--- Mesa.java
+++ Mesa.java
@@ -1,42 +1,50 @@
 public class Mesa {
-  private int num_mesa;
-  private int capacidade;
-  private boolean status;
+    private int num_mesa;
+    private int capacidade;
+    private boolean status;
 
-  public Mesa(int num_mesa, int capacidade) {
-      this.num_mesa = num_mesa;
-      this.capacidade = capacidade;
-      this.status = false;
-  }
+    public Mesa(int num_mesa, int capacidade) {
+        this.num_mesa = num_mesa;
+        this.capacidade = capacidade;
+        this.status = false;
+    }
 
-  public int getNumMesa() {
-      return num_mesa;
-  }
-  public int getCapacidade() {
-      return capacidade;
-  }
-  public boolean getStatus() {
-      return status;
-  }
-  public void setNum_mesa(int num_mesa) {
-    this.num_mesa = num_mesa;
-}
-public void setCapacidade(int capacidade) {
-    this.capacidade = capacidade;
-}
+    public int getNumMesa() {
+        return num_mesa;
+    }
 
-  public void consultarStatus() {
-      if (status) {
-          System.out.println("A mesa " + this.num_mesa + " está disponível");
-      } else {
-          System.out.println("A mesa " + this.num_mesa + " não está disponível");
-      }
-  }
+    public int getCapacidade() {
+        return capacidade;
+    }
 
-  public void mudarStatus() {
-      status = !status;
-      System.out.println("A mesa " + this.num_mesa + (status ? " ficou disponível" : " não está mais disponível"));
-  }
+    public boolean getStatus() {
+        return status;
+    }
 
-    
+    public void setNum_mesa(int num_mesa) {
+        this.num_mesa = num_mesa;
+    }
+
+    public void setCapacidade(int capacidade) {
+        this.capacidade = capacidade;
+    }
+
+    public void consultarStatus() {
+        if (status == false) {
+            System.out.println("A mesa " + this.num_mesa + " está disponível");
+        } else {
+            System.out.println("A mesa " + this.num_mesa + " não está disponível");
+        }
+    }
+
+    public void mudarStatus() {
+        if (status == false) {
+            status = true;
+            System.out.println("A mesa " + this.num_mesa + " não está mais disponível");
+        } else {
+            status = false;
+            System.out.println("A mesa " + this.num_mesa + " agora está disponível");
+        }
+    }
+
 }
--- Restaurante.java
+++ Restaurante.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class Restaurante {
 
-  public static List<Cardapio> cardapio() {
-    List<Cardapio> cardapio = new ArrayList<>();
+  public static void main(String[] args) {
 
+    List<Cardapio> cardapio = new ArrayList<>();
     cardapio.add(new Cardapio("Jen's Cheese", 1, 24.00));
     cardapio.add(new Cardapio("Jen's Salada", 2, 26.00));
     cardapio.add(new Cardapio("Jen's Cheddar", 3, 26.00));
@@ -22,20 +22,7 @@ public class Restaurante {
     cardapio.add(new Cardapio("Jen's Costela Alhoó", 13, 37.90));
     cardapio.add(new Cardapio("Jen's Costela BBQ", 14, 38.80));
     cardapio.add(new Cardapio("Duplo BCC", 15, 39.90));
-    return cardapio;
-  }
 
-  public static List<Mesa> mesas() {
-    List<Mesa> mesas = new ArrayList<>();
-    mesas.add(new Mesa(1, 4));
-    mesas.add(new Mesa(2, 4));
-    mesas.add(new Mesa(3, 4));
-    mesas.add(new Mesa(4, 6));
-    mesas.add(new Mesa(5, 10));
-    return mesas;
-  }
-
-  public static List<Funcionario> funcionarios() {
     List<Funcionario> funcionarios = new ArrayList<>();
     funcionarios.add(new Funcionario("Cleiton", 1000, "Garçom"));
     funcionarios.add(new Funcionario("Robson", 1001, "Garçom"));
@@ -43,17 +30,18 @@ public class Restaurante {
     funcionarios.add(new Funcionario("Maria", 3000, "Cozinheiro"));
     funcionarios.add(new Funcionario("Vitor", 3001, "Cozinheiro"));
     funcionarios.add(new Funcionario("Danilo", 1002, "Garçom"));
-    return funcionarios;
-  }
 
-  public static void main(String[] args) {
-
-    mesas().get(1).mudarStatus();
+    List<Mesa> mesas = new ArrayList<>();
+    mesas.add(new Mesa(1, 4));
+    mesas.add(new Mesa(2, 4));
+    mesas.add(new Mesa(3, 4));
+    mesas.add(new Mesa(4, 6));
+    mesas.add(new Mesa(5, 10));
 
     while (true) {
       Scanner scan = new Scanner(System.in);
       int op;
-      System.out.println("-------- MENU ---------");
+      System.out.println("\n-------- MENU ---------");
       System.out.println("1 -- Consultar mesa");
       System.out.println("2 -- Mudar o Status da mesa");
       System.out.println("3 -- Mostrar cardapio");
@@ -63,16 +51,25 @@ public class Restaurante {
 
       switch (op) {
         case 0 -> {
-          break;
+          return;
         }
         case 1 -> {
           System.out.println("Digite o numero da mesa que deseja consultar:");
           op = scan.nextInt();
-          mesas().get(op-1).consultarStatus();
+          mesas.get(op - 1).consultarStatus();
+        }
+        case 2 -> {
+          System.out.println("Digite o numero da mesa que deseja mudar o status:");
+          op = scan.nextInt();
+          mesas.get(op - 1).mudarStatus();
+        }
+        case 3 -> {
+          System.out.println("\n--- Cardapio ---");
+          for(Cardapio item : cardapio){
+            System.out.println("- " + item.getNomePratoBebida() + ": R$ " + item.getPreco());
+          }
         }
       }
-
     }
-
   }
 }
